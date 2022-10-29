d31cee4f59bd013c1636432c333081feeb5aac5d
Alessandro Peria <alessandroperia@icloud.com>
2022-10-29 19:53:22 +0200

    Fix hash_function_t and add jnp1 namespace

--- hash.h
+++ hash.h
@@ -4,14 +4,15 @@
 #include <cstddef>
 #include <cstdint>
 
-using hash_function_t = uint64_t(*)(uint64_t*, size_t);
+using hash_function_t = uint64_t(*)(uint64_t const *, size_t);
 
-unsigned long hash_create(hash_function_t hash_function);
-void hash_delete(unsigned long id);
-size_t hash_size(unsigned long id);
-bool hash_insert(unsigned long id, uint64_t const * seq, size_t size);
-bool hash_remove(unsigned long id, uint64_t const * seq, size_t size);
-void hash_clear(unsigned long id);
-bool hash_test(unsigned long id, uint64_t const * seq, size_t size);
-
+namespace jnp1{
+    unsigned long hash_create(hash_function_t hash_function);
+    void hash_delete(unsigned long id);
+    size_t hash_size(unsigned long id);
+    bool hash_insert(unsigned long id, uint64_t const * seq, size_t size);
+    bool hash_remove(unsigned long id, uint64_t const * seq, size_t size);
+    void hash_clear(unsigned long id);
+    bool hash_test(unsigned long id, uint64_t const * seq, size_t size);
+}
 #endif
